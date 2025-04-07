32173fbbc01126e8db139ccab1f309a918a6303a
juan.suarez@hyve.net <juan.suarez@hyve.net>
2025-04-07 11:20:14 +0200

    eliminacion de ficheros redundantes para la prueba

--- src/App.tsx
+++ src/App.tsx
@@ -1,17 +1,9 @@
 import React, { JSX} from 'react';
-import { IconManagerProvider} from './context/IconManagerProvider';
-import IconList from './components/IconList/IconList';
-import './index.css';
-import HeaderContainer from './components/Header/HeaderContainer';
+
 
 const App = ():JSX.Element => {
   return (
-    <IconManagerProvider>
-      <div className="app">
-        <HeaderContainer/>
-        <IconList/>
-      </div>
-    </IconManagerProvider>
+    <div>hola</div>
   );
 };
 
